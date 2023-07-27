6d9bf65b45609e840b4fd4d034b3fb21164e89dd
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-28 02:09:13 +0530

    Unit Testing Service Layer

---
+++ src/main/java/com/attraya/springboot/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.attraya.springboot.service;
+
+import com.attraya.springboot.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(long id);
+
+    Employee updateEmployee(Employee updatedEmployee);
+
+    void deleteEmployee(long id);
+}
