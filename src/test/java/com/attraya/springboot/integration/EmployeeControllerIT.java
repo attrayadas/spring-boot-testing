fc094e67d1955ba6d421287d69f2eb8451e8bcf2
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-29 23:40:42 +0530

    Using @DynamicPropertySource for Integration Testing using TestContainers

--- src/test/java/com/attraya/springboot/integration/EmployeeControllerIT.java
+++ src/test/java/com/attraya/springboot/integration/EmployeeControllerIT.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.MySQLContainer;
@@ -33,6 +35,13 @@ public class EmployeeControllerIT {
             .withUsername("username") // defining user defined credentials
             .withPassword("password")
             .withDatabaseName("ems");
+
+    @DynamicPropertySource
+    public static void dynamicPropertySource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
     @Autowired
     private MockMvc mockMvc;
